fbad55b1d05155f9c399266926a112bb1af1e01c
Nikhil CSB <csbnikhil@gmail.com>
2020-08-02 23:27:22 +0530

    add more punc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    #[regex("(int|float|char)")]
+    Keyword,
+
+    #[regex("(:|;|,|\\[|\\]|\\(|\\))")]
+    Punctuation,
+
+    #[regex("[0-9]+.?[0-9]*")]
+    Number,
+
+    #[regex("[ \t\n]+")]
+    Whitespace,
+
+    #[regex("[_a-zA-Z][a-zA-Z_0-9]*")]
+    Identifier,
+
+    #[error]
+    Error,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let source = "int main(int argc, char *argv[])";
+    let mut lexer = Token::lexer(source);
+
+    while let Some(t) = lexer.next() {
+        println!("{:?}", t);
+    }
 }
